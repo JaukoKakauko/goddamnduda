7154c598716e9afbc931ad2778bc34df6ae5768f
JaukoKakauko <jarko.petrun@gmail.com>
2024-10-01 09:38:15 +0200

    prva funkcna vec

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [ProfileController::class, 'index']);
