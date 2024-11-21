2f63a4dc1b1cc4f1e6de9e2025bc94f66f3c9439
JaukoKakauko <jarko.petrun@gmail.com>
2024-11-21 11:59:54 +0100

    niggaaaaaaaaaaaaaaaaaaaaaaa

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function store() {
+        
+        $request =(request()->all());
+        Post::create([
+            "content" => $request["content"],
+            "likes" => 0,
+        ]);
+
+        return redirect()->route("dashboard.index");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index']);
-//Route::get('/', [ProfileController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])-> name("dashboard.index");
+Route::post('/post', [PostController::class, 'store'])-> name("post.store");
+
